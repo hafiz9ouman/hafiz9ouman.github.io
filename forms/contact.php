f74e2ce1e4eb162b5e52b569e203a0e51de72a7c
stepinnsolution2 <stepinnsolution@gmail.com>
2023-05-09 14:18:37 +0500

    Mail Config

--- forms/contact.php
+++ forms/contact.php
@@ -21,9 +21,9 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'hafiz9oman.dev@gmail.com',
+    'password' => 'wnmiccubcusxnszw',
     'port' => '587'
   );
 
